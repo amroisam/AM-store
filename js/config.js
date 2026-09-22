af7397547d514679d1d5c8879a9940202464eb3a
amroisam <120948221+amroisam@users.noreply.github.com>
2026-09-22 12:02:54 +0300

    Initialize Supabase client in config.js

---
+++ js/config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://uacnpssrurwhrshmsnja.supabase.co"; // ضع رابط مشروعك هنا
+const SUPABASE_ANON_KEY = "sb_publishable_0iqwsODpmzEwIGHeggqaUw_30lpY68V"; // ضع مفتاح anon هنا
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
